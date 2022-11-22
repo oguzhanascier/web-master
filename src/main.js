4af8069d37ad05c5207b96adac1387156abdfad1
oguzhanascier <oguzhanascier20@gmail.com>
2022-11-22 13:09:25 +0300

    fail

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
-import VueToastify from "vue-toastify";
 import "./style/main.css";
+import VueToastify from "vue-toastify";
 Vue.use(VueToastify);
 
 Vue.config.productionTip = false;
