e11e1c8db72cfe6172af2a8fd5a3106322d5ca2e
oguzhanascier <oguzhanascier20@gmail.com>
2022-11-26 22:11:34 +0300

    ToDo Component import

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,65 @@
 <template>
-    <div class="container-fluid" :class="{ neuCont: morp }">
-    <Container :setData="cardList" :dptoggle="toggle"  @moon="morp = $event"></Container>
-    <NewItem @reData="cardList.push($event)" @toggle="toggle=$event" :itemMorp="morp" ></NewItem>
-  </div>
-  <!-- <Home></Home> -->
-
+    
+    <!-- <div class="container-fluid" :class="{ neuCont: morp }">
+      <Container
+        :setData="cardList"
+        :dptoggle="toggle"
+        @moon="morp = $event"
+      ></Container>
+      <NewItem
+        @reData="cardList.push($event)"
+        @toggle="toggle = $event"
+        :itemMorp="morp"
+      ></NewItem>
+    </div>
+  -->
+    <ToDo></ToDo>
+  
 </template>
 
 <script>
-import Container from "./components/Container.vue";
-import NewItem from "./components/NewItem.vue";
-import Home from "./components/Home.vue";
-export default {
-  components: {
-    Container,
-    NewItem,
-    Home,
-  },
-  data() {
-    return {
-      cardList: [],
-      morp: null,
-      toggle: false,
-    };
-  },
+import ToDo from './components/ToDo.vue'
+export default{
+  components:{
+    ToDo
+  }
+}
+// import ToDo from './components/ToDo.vue'
+// import Container from "./components/Container.vue";
+// import NewItem from "./components/NewItem.vue";
+// import Home from "./components/Home.vue";
+// export default {
+//   components: {
+//     Container,
+//     NewItem,
+//     Home,
+//     ToDo
+//   },
+//   data() {
+//     return {
+//       cardList: [],
+//       morp: null,
+//       toggle: false,
+//     };
+//   },
 
-  mounted() {
-    if (localStorage.cardList) {
-      this.cardList = JSON.parse(localStorage.cardList);
-    }
-    if (localStorage.cssMorp) {
-      this.morp = JSON.parse(localStorage.cssMorp);
-    }
-  },
-  watch: {
-    cardList(newNotes) {
-      localStorage.cardList = JSON.stringify(newNotes);
-    },
-  },
-};
+//   mounted() {
+//     if (localStorage.cardList) {
+//       this.cardList = JSON.parse(localStorage.cardList);
+//     }
+//     if (localStorage.cssMorp) {
+//       this.morp = JSON.parse(localStorage.cssMorp);
+//     }
+//   },
+//   watch: {
+//     cardList(newNotes) {
+//       localStorage.cardList = JSON.stringify(newNotes);
+//     },
+//   },
+// };
 </script>
 
 <style>
-
 .container-fluid {
   background: url(./assets/shapelined-_JBKdviweXI-unsplash.jpg);
   background-position: center center;
@@ -51,7 +68,4 @@ export default {
   background-repeat: no-repeat;
   transition: 0.3s ease-in-out;
 }
-
-
-
 </style>
